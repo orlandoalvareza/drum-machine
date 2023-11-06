004ea805084ca583d4b950b8c8589c63a16080f2
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-11-06 01:16:24 -0500

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 function App() {
   return (
     <div className="App">
-      Hello
+      <div id="drum-machine">
+        <div id="display">display</div>
+        <div className="controls-container">
+          <div id="volume-slider">
+            <h4 id="volume-header">Volume Control</h4>
+            <input/>
+          </div>
+          <div id ='power-container'>
+            <button></button>
+          </div>
+        </div>
+        <div className="pad-bank">
+          <button>
+            <audio/>
+            Q
+          </button>
+          <button>
+            <audio/>
+            W
+          </button>
+          <button>
+            <audio/>
+            E
+          </button>
+          <button>
+            <audio/>
+            A
+          </button>
+          <button >
+            <audio/>
+            S
+          </button>
+          <button>
+            <audio/>
+            D
+          </button>
+          <button >
+            <audio/>
+            Z
+          </button>
+          <button>
+            <audio/>
+            X
+          </button>
+          <button>
+            <audio/>
+            C
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
